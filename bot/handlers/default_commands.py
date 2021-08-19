3bc431c9a597c670ea7e4a7246ebfb539bae1f6d
Groosha <groosha@protonmail.com>
2021-08-19 23:30:08 +0300

    /start command

--- bot/handlers/default_commands.py
+++ bot/handlers/default_commands.py
@@ -1,8 +1,14 @@
 from aiogram import Dispatcher, types
+from aiogram.dispatcher import FSMContext
 
 
-async def cmd_start(message: types.Message):
-    await message.answer("Обработка /start")
+async def cmd_start(message: types.Message, state: FSMContext):
+    # показ клавиатуры с кнопкой start
+    kb = types.InlineKeyboardMarkup()
+    kb.add(types.InlineKeyboardButton(text="Start Game!", callback_data="newgame"))
+    await message.answer("Press the button below to start a new game (previous one will be dismissed).\n"
+                         "For now you cannot choose any options, so a 5x5 field will be used",
+                         reply_markup=kb)
 
 
 async def cmd_help(message: types.Message):
