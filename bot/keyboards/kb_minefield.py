38f82595886d42151eab227140cb3f10719d14cb
Groosha <groosha@protonmail.com>
2021-08-24 22:48:33 +0300

    Changed keyboard generation

--- bot/keyboards/kb_minefield.py
+++ bot/keyboards/kb_minefield.py
@@ -3,44 +3,36 @@ from typing import List
 from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
 from bot.cbdata import cb_switch_mode, cb_click, cb_switch_flag
-from minesweeper.generators import generate_square_field
 from minesweeper.states import ClickMode, MaskFieldSquareStatus
 
 
-def make_keyboard_from_minefield(minefield: List[List], fieldmask: List[List], game_id: str, click_mode: int):
-    """
-
-    :param minefield:
-    :param fieldmask:
-    :param game_id:
-    :param click_mode:
-    :return:
-    """
-    kb_field = generate_square_field(len(minefield))
-    for y in range(len(minefield)):
-        for x in range(len(minefield[0])):
-            fieldmask_value = fieldmask[x][y]
-            if fieldmask_value == MaskFieldSquareStatus.OPEN:
-                kb_field[x][y] = InlineKeyboardButton(text=minefield[x][y], callback_data="ignore")
-            elif fieldmask_value == MaskFieldSquareStatus.HIDDEN:
-                btn = InlineKeyboardButton(text="•")
+def make_keyboard_from_minefield(cells: List[List], game_id: str, click_mode: str) -> InlineKeyboardMarkup:
+    keyboard = InlineKeyboardMarkup()
+    for cells_row in cells:
+        kb_row = []
+        for cell in cells_row:
+            # This is a button for OPEN status
+            btn = InlineKeyboardButton(text=cell["value"], callback_data="ignore")
+            mask_value = cell["mask"]
+            x = cell["x"]
+            y = cell["y"]
+            # Check other statuses
+            if mask_value == MaskFieldSquareStatus.HIDDEN:
+                btn.text = "•"
                 if click_mode == ClickMode.CLICK:
                     btn.callback_data = cb_click.new(game_id=game_id, x=x, y=y)
                 else:
                     btn.callback_data = cb_switch_flag.new(game_id=game_id, action="add", x=x, y=y)
-                    # btn.callback_data = cb_flag.new(game_id=game_id, x=x, y=y)
-                kb_field[x][y] = btn
-            elif fieldmask_value == MaskFieldSquareStatus.BOMB:
-                kb_field[x][y] = InlineKeyboardButton(text="💥", callback_data="ignore")
-            elif fieldmask_value == MaskFieldSquareStatus.FLAG:
-                kb_field[x][y] = InlineKeyboardButton(
-                    text="🚩",
-                    callback_data=cb_switch_flag.new(game_id=game_id, action="remove", x=x, y=y)
-                )
+            # todo: remove BOMB button and replace with in-text field
+            elif mask_value == MaskFieldSquareStatus.BOMB:
+                btn.text = "💥"
+            elif mask_value == MaskFieldSquareStatus.FLAG:
+                btn.text = "🚩"
+                btn.callback_data = cb_switch_flag.new(game_id=game_id, action="remove", x=x, y=y)
+            kb_row.append(btn)
+        keyboard.row(*kb_row)
 
-    result_kb = InlineKeyboardMarkup(inline_keyboard=kb_field)
-
-    # Add switch mode button
+    # Add switch flag mode button
     if click_mode == ClickMode.FLAG:
         switch_mode_btn = InlineKeyboardButton(
             text="🔄 Current mode: Flag",
@@ -49,5 +41,6 @@ def make_keyboard_from_minefield(minefield: List[List], fieldmask: List[List], g
         switch_mode_btn = InlineKeyboardButton(
             text="🔄 Current mode: Click",
             callback_data=cb_switch_mode.new(game_id=game_id, new_mode=ClickMode.FLAG))
-    result_kb.row(switch_mode_btn)
-    return result_kb
+    keyboard.row(switch_mode_btn)
+
+    return keyboard
