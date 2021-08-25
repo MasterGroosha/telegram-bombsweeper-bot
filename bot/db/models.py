d9862daca19f0e6f244503a455e5d833cfafe786
Groosha <groosha@protonmail.com>
2021-08-26 00:43:11 +0300

    Added GameHistory model for SQLAlchemy

---
+++ bot/db/models.py
@@ -0,0 +1,13 @@
+from sqlalchemy import Column, Integer, BigInteger, Boolean
+from sqlalchemy.dialects.postgresql import UUID
+
+from bot.db.base import Base
+
+
+class GameHistoryEntry(Base):
+    __tablename__ = "gameshistory"
+
+    game_id = Column(UUID, primary_key=True)
+    telegram_id = Column(BigInteger, nullable=False)
+    field_size = Column(Integer, nullable=False)
+    victory = Column(Boolean, nullable=False)
