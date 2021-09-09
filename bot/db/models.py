03158227a59b6a6bbe1ed8d8495d80abf5d6f8c5
Groosha <groosha@protonmail.com>
2021-09-09 19:03:24 +0300

    Create index for telegram_id column

--- bot/db/models.py
+++ bot/db/models.py
@@ -9,6 +9,6 @@ class GameHistoryEntry(Base):
 
     game_id = Column(UUID, primary_key=True)
     played_at = Column(DateTime, nullable=False)
-    telegram_id = Column(BigInteger, nullable=False)
+    telegram_id = Column(BigInteger, nullable=False, index=True)
     field_size = Column(Integer, nullable=False)
     victory = Column(Boolean, nullable=False)
