6c7d1904ed23e0ee2f36a1346971c8b2f7df658f
Groosha <groosha@protonmail.com>
2022-09-17 19:18:57 +0300

    Updated tests

--- tests/test_cells_counters.py
+++ tests/test_cells_counters.py
@@ -7,6 +7,7 @@ from bot.minesweeper import game
 from tests.test_data.minefields import \
     game_won, one_open_cell, game_in_progress, \
     all_flags_correct_has_hidden_cells, more_flags_than_should
+from bot.minesweeper.states import GameState
 
 
 def count_items(rows, count_func: Callable) -> int:
@@ -14,21 +15,17 @@ def count_items(rows, count_func: Callable) -> int:
 
 
 @pytest.mark.parametrize(
-    "field, all_free_cells_are_open, untouched_cells_count, all_flags_match_bombs",
+    "field, game_state",
     [
-        [game_won, True, 3, True],
-        [one_open_cell, False, 5*5-1, True],
-        [game_in_progress, False, 2, True],
-        [all_flags_correct_has_hidden_cells, False, 1, True],
-        [more_flags_than_should, False, 0, False],
+        [game_won, GameState.VICTORY],
+        [one_open_cell, GameState.HAS_HIDDEN_NUMBERS],
+        [game_in_progress, GameState.HAS_HIDDEN_NUMBERS],
+        [all_flags_correct_has_hidden_cells, GameState.HAS_HIDDEN_NUMBERS],
+        [more_flags_than_should, GameState.MORE_FLAGS_THAN_BOMBS],
     ]
 )
 def test_fields(
         field: List[List[Dict]],
-        all_free_cells_are_open: bool,
-        untouched_cells_count: int,
-        all_flags_match_bombs: bool
+        game_state: int
 ):
-    assert game.all_free_cells_are_open(field) == all_free_cells_are_open
-    assert game.untouched_cells_count(field) == untouched_cells_count
-    assert game.all_flags_match_bombs(field) == all_flags_match_bombs
+    assert game.analyze_game_field(field) == game_state
